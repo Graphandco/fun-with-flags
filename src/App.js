284cef3bd7c9779412ab79c65495d78edc45cb6b
Reggio <contact@graphandco.com>
2020-07-23 19:37:35 +0200

    Correction

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.scss';
 import Header from './Components/Header';
 import FlagsList from './Components/FlagsList';
